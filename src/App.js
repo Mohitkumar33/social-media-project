50f872e5186ffa0f4949f7a1d0550d8c12559f69
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-05-04 13:37:44 +0530

    chore: done initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Social media starting</div>
+    </div>
+  );
+}
+
+export default App;
